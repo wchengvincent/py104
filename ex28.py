44565fb89a61aaca2502c3e8c9d7e04091eadae0
wchengvincent <wchengvincent@yahoo.com>
2017-08-03 21:56:53 +0800

    First Time

---
+++ ex28.py
@@ -0,0 +1,61 @@
+
+#1
+print True and True
+
+#2
+print False and True
+
+#3
+print 1 == 1 and  2 == 1
+
+#4
+print "test" == "test"
+
+#5
+print 1 == 1 or 2 != 1
+
+#6
+print True and 1 == 1
+
+#7
+print False and 0 != 0
+
+#8
+print True or 1 == 1
+
+#9
+print "test" == "testing"
+
+#10
+print 1 != 0 and 2 == 1 
+
+#11
+print "test" != "testing"
+
+#12
+print "test"  == 1
+
+#13
+print not (True and False)
+
+#14
+print not (1 == 1 and 0 != 1)
+
+#15
+print not (10 == 1 or 1000 == 1000)
+
+#16
+print not (1 != 10 or 3 == 4)
+
+#17
+print not ("testing" == "testing" and "Zed" == "Cool Guy")
+
+#18
+print 1 == 1 and not ("testing" == 1 or 1 == 0)
+
+#19
+print "chunky" == "bacon" and not (3 == 4 or 3 == 3)
+
+#20
+print 3 == 3 and not ("testing" == "test-ing" or "Python" == "Fun")
+
